ccb4354d3eb7e12001327b2441f649657e12e7ea
chandrakishor <chandrakishoret197@gmail.com>
2025-04-06 23:14:03 +0530

    Singup data send html into php

--- client/singup.php
+++ client/singup.php
@@ -1,24 +1,24 @@
 <div class="container">
    <h1 class="heading">Singup</h1> 
-   <form action="" method="get">
+   <form method="post" action="./server/request.php" >
    <div class="col-6 offset-sm-3 margin-15">
   <label for="username" class="form-label">Name</label>
-  <input type="text" class="form-control" id="username" placeholder="Enter your name">
+  <input type="text" name="username" class="form-control" id="username" placeholder="Enter your name">
 </div>
 <div class="col-6 offset-sm-3 margin-15">
   <label for="Email" class="form-label">Email address</label>
-  <input type="text" class="form-control" id="email" placeholder="Enter your Eamil">
+  <input type="text" name="email" class="form-control" id="email" placeholder="Enter your Eamil">
 </div>
 <div class="col-6 offset-sm-3 margin-15">
   <label for="Password" class="form-label">Password</label>
-  <input type="text" class="form-control" id="password" placeholder="Enter The Password">
+  <input type="text" name="password" class="form-control" id="password" placeholder="Enter The Password">
 </div>
 <div class="col-6 offset-sm-3 margin-15">
   <label for="addresh" class="form-label">Addresh</label>
-  <textarea class="form-control" id="addresh" rows="3"></textarea>
+  <textarea class="form-control" name="addresh" id="addresh" rows="3"></textarea>
 </div>
 <div class="col-6 offset-sm-3 margin-15">
-    <button type="submit" class="btn btn-primary mb-3">Submit</button>
+    <button type="submit" name="singup" class="btn btn-primary mb-3">Singup</button>
   </div>
    </form>
 </div>
---
+++ server/request.php
@@ -0,0 +1,10 @@
+<?php 
+
+if(isset($_POST['singup'])){
+    echo "username is"." ".$_POST['username']."<br>";
+    echo "user email id is"." ".$_POST['email']."<br>";
+    echo "user password is"." ".$_POST['password']."<br>";
+    echo "user addresh is"." ".$_POST['addresh']."<br>";
+}
+
+?>
